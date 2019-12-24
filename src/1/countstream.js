80d040829306c1f790eecf7c4008f27d0b7a7d5c
dibashi <dibashi1989@163.com>
2019-12-24 22:22:17 +0800

    11

--- src/1/countstream.js
+++ src/1/countstream.js
@@ -1,24 +1,14 @@
-var Writable = require('stream').Writable;
-var util = require('util');
+// class EventEmitter {
+// 	constructor() {
+// 		if (this._events === undefined) {
+// 			this._events = Object.create(null);//定义事件对象
+// 			this._eventsCount = 0;
+// 		}
+// 	}
+// }
 
-module.exports = CountStream;
+var a = Object.create(null);
+var b = {};
 
-util.inherits(CountStream, Writable);
-
-function CountStream(matchTxt, options) {
-	Writable.call(this, options);
-	this.count = 0;
-	this.matcher = new RegExp(matchTxt, 'ig');
-}
-
-CountStream.prototype._write = function(chunk, encodeing, cb) {
-	var matchs = chunk.toString().match(this.matcher);
-	if (matchs) {
-		this.count += matchs.length;
-	}
-	cb();
-};
-
-CountStream.prototype.end = function() {
-	this.emit('total', this.count);
-};
+console.log(a);
+console.log(b);
--- src/1/test-1.js
+++ src/1/test-1.js
@@ -1,16 +1,25 @@
-var assert = require('assert');
-var CountStream = require('./countStream');
-var countStream = new CountStream('example');
-var fs = require('fs');
-var passed = 0;
 
-countStream.on('tatal', function(count){
-	assert.equal(count, 1);
-	passed++;
-});
 
-fs.createReadStream(__filename).pipe(countStream);
+function My_EventEmitter() {
+	this.handles = {};
+	this.handles["newListener"] = [];
+}
 
-process.on('exit', function() {
-	console.log('Assertions passed: ', passed);
-});
+My_EventEmitter.prototype.addEventListener = function (eventName, listener) {
+	if (!this.handles[eventName]) {
+		this.handles[eventName] = [];
+	}
+
+	this.handles[eventName].push(listener);
+	this.emit("newListener");
+}
+
+My_EventEmitter.prototype.on = My_EventEmitter.prototype.addEventListener;
+
+My_EventEmitter.prototype.emit = function (eventName, ...args) {
+	if (this.handles[eventName]) {
+		for (let i = 0, len = this.handles[eventName].length; i < len; i++) {
+			this.handles[eventName][i](...args);
+		}
+	}
+}
